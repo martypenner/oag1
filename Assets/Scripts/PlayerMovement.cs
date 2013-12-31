1ecfc9365fc90f9c0bb0a827038f31a2ec9796cd
Marty Penner <marty@gotenzing.com>
2013-12-31 00:07:33 -0500

    Sprint 1 (basic character movement)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Transform planet;
+	public float speed = 25f;
+//	public float rotateSpeed = 45f;
+
+	void FixedUpdate ()
+	{
+		float horz = -Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		transform.RotateAround (planet.position, transform.right, speed * Time.deltaTime * vert);
+//		transform.Rotate (0f, rotateSpeed * Time.deltaTime * horz, 0f);
+		transform.RotateAround (planet.position, transform.forward, speed * Time.deltaTime * horz);
+	}
+}
